61756d6cc66e684bb0ec027cf1a406d38553cb70
Izzy Hymas <izzyhymas@gmail.com>
2024-04-19 11:34:08 -0600

    Add database

--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
 
-from models import Todo
+import models
+import schemas
+import crud
 
+from database import SessionLocal, engine
+
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-todos: list[Todo] = []
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
+@app.get("/", response_model=list[schemas.Todo])
+async def get_todos(db: Session = Depends(get_db)) -> list[schemas.Todo]:
+    return crud.get_todos(db=db)
 
-@app.get("/")
-async def get_todos() -> list[Todo]:
-    return todos
-
-@app.post("/")
-async def create_todo(todo: Todo) -> None:
-    todos.append(todo)
+@app.post("/", response_model=schemas.Todo)
+async def create_todo(todo: schemas.TodoCreate, db: Session = Depends(get_db)) -> schemas.Todo:
+    return crud.create_todo(db=db, todo=todo)
